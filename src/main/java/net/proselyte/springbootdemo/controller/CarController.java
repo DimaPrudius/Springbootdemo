8e191c31d55c2b401c8a9cf19b64eab4918ce843
DimaPrudius <dimaprudius1996@gmail.com>
2021-11-09 16:28:57 +0200

    Init

--- src/main/java/net/proselyte/springbootdemo/controller/CarController.java
+++ src/main/java/net/proselyte/springbootdemo/controller/CarController.java
@@ -2,7 +2,7 @@ package net.proselyte.springbootdemo.controller;
 
 import net.proselyte.springbootdemo.model.Car;
 import net.proselyte.springbootdemo.service.CarService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,33 +21,38 @@ public class CarController {
     }
 
     @GetMapping("/cars")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<Car> cars = carService.findAll();
-        model.addAttribute("cars",cars);
+        model.addAttribute("cars", cars);
         return "car-list";
     }
+
     @GetMapping("/car-create")
-    public String creteCarForm(Car car){
+    public String creteCarForm(Car car) {
         return "car-create";
     }
+
     @PostMapping("/car-create")
-    public String createCar(Car car){
+    public String createCar(Car car) {
         carService.saveCar(car);
         return "redirect:/cars";
     }
+
     @GetMapping("car-delete/{id}")
-    public String deleteCar(@PathVariable("id") Long id){
+    public String deleteCar(@PathVariable("id") Long id) {
         carService.deleteById(id);
         return "redirect:/cars";
     }
+
     @GetMapping("/car-update/{id}")
-    public String updateCarForm(@PathVariable("id") Long id,Model model){
+    public String updateCarForm(@PathVariable("id") Long id, Model model) {
         Car car = carService.findById(id);
-        model.addAttribute("car",car);
+        model.addAttribute("car", car);
         return "/car-update";
     }
+
     @PostMapping("/car-update")
-    public  String updateCar(Car car){
+    public String updateCar(Car car) {
         carService.saveCar(car);
         return "redirect:/cars";
     }
